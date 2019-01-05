d232d01bb2a90e3b0b8f7bd656f382729f6b69d9
sillysimon <muda99@gmx.de>
2019-01-05 20:20:51 +0100

    tried to fix it (didn't work)

--- Counter/timer.py
+++ Counter/timer.py
@@ -6,4 +6,10 @@ hrs = 0
 while True:
     while sec <= 60:
         print hrs , ":" , min, ":" , sec
-        sec 
+        sec += 1
+        time.sleep(1)
+ sec == 0
+ min += 1
+ if min == 60:
+     min == 0
+     hrs += 1
