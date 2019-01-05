a998cfdbe1ea6b7513aba1e2314a5e65f23c0536
sillysimon <muda99@gmx.de>
2019-01-05 19:29:40 +0100

    Added Objects

---
+++ Objects/variables.py
@@ -0,0 +1,10 @@
+#in Python, it seems that you dont have to specify the type of an obeject
+#you just have to say the name and give it a value like this:
+number = 2
+this_is_a_float = 1.1
+this_is_a_string = "String"
+#you can also do operations with objects
+one = 1
+two = 2
+three = one + two
+print "The answer is", three
